f155658641c269ec9a2b533b2146503c569d3a4d
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-19 17:47:44 +0530

    feat: add HeroSection component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import { StatsSection } from "@/components/stats-section"
 import { BrandKitsSection } from "@/components/brand-kits-section"
 import { CarbonEmissionsSection } from "@/components/carbon-emissions-section"
 import CustomerSay from "@/components/CustomerSay"
+import { HeroSection } from "@/components/herosection"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background transition-colors duration-300">
       <Navbar />
       <main className="container mx-auto px-3 py-8 space-y-12">
+        <HeroSection/>
         <StatsSection />
         <BrandKitsSection />
         <CarbonEmissionsSection />
